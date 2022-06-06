6d075219c065b50e747e83ad4adb872b1219cd12
Carter Dennis <carter.j.dennis@gmail.com>
2022-06-06 12:50:49 -0500

    Add gitignore to hide generated codes

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import qrcode
-import pandas
+import qrcode, pandas
 
 df = pandas.read_excel('CRP_Spreadsheet.xlsx')
 
 for ind in df.index:
     img = qrcode.make('google.com/maps/place/' + str(df['Latitude'][ind]) + ',' + str(df['Longitude'][ind]))
-    type(img)
     img.save('codes/' + df['Farm and Tract No.'][ind] + '.png')
