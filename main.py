27ad196da06537fdb3d0fda85f155de2004cf59d
Carter Dennis <carter.j.dennis@gmail.com>
2022-06-06 16:54:58 -0500

    Added https to link for Android usability

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import qrcode, pandas
 df = pandas.read_excel('CRP_Spreadsheet.xlsx')
 
 for ind in df.index:
-    img = qrcode.make('google.com/maps/place/' + str(df['Latitude'][ind]) + ',' + str(df['Longitude'][ind]))
+    img = qrcode.make('https://www.google.com/maps/place/' + str(df['Latitude'][ind]) + ',' + str(df['Longitude'][ind]))
     img.save('codes/' + df['Farm and Tract No.'][ind] + '.png')
